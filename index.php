283c230427f975c1e2666ab0520bb21bf1bb630b
VavraLuka <luka123vavra@gmail.com>
2023-03-15 19:57:57 +0100

    Update

--- index.php
+++ index.php
@@ -6,47 +6,66 @@
 </head>
 <body>
     <div class="page-wrapper">
-        <!--  -->
-        <section>
+        <!-- Section 01 -->
+        <section class="background-grey">
             <div class="section-wrapper">
-                <div class="left">
-                    <ul>
+                <div class="box">
+                    <ul class="text-black">
                         <li><a href="#contact">Kontaktiraj nas</a></li>
                         <li><a href="#about">O nama</a></li>
                     </ul>
                 </div>
-                <div class="center">
+                <div class="box main-logo">
                     <p><a href="#index">Logo</a></p>
                 </div>
-                <div class="right">
-                    <ul>
+                <div class="box">
+                    <ul class="float-right text-black">
                         <li><a href="#warranty">3 godine jamstva</a></li>
                         <li><a href="#returns">Povrat robe</a></li>
                     </ul>
                 </div>
             </div>
-        </section>
-
-        <section>
             <div class="section-wrapper">
-                <div>
-                    <ul>
-                        <li><a>Top ponude</a></li>
-                        <li><a>Novo</a></li>
-                        <li><a>Najprodavanije</a></li>
+                <div class="box">
+                    <ul class="bold-text text-black">
+                        <li><a href="#topdeals">Top ponude</a></li>
+                        <li><a href="#new">Novo</a></li>
+                        <li><a href="#mostpopular">Najprodavanije</a></li>
                     </ul>
                 </div>
-                <div>
+                <div class="box search-bar">
                     <p>Search bar</p>
                 </div>
-                <div>
-                    <ul>
-                        <li><a>Odabir valute</a></li>
-                        <li><a>Profil</a></li>
-                        <li><a>Favoriti</a></li>
-                        <li><a>Košarica</a></li>
+                <div class="box">
+                    <ul class="float-right text-black">
+                        <li><a href="#currencychange">Odabir valute</a></li>
+                        <li><a href="#cart">Prijava</a></li>
+                        <li><a href="#cart">Registracija</a></li>
+                        <!-- <li><a href="#favorites">Favoriti</a></li>
+                        <li><a href="#cart">Košarica</a></li>
+                        <li><a href="#myprofile">Profil</a></li>
+                        <li><a href="#myprofile">Odjava</a></li> -->
                     </ul>
                 </div>
+            </div>
+        </section>
+
+        <!-- Section 02 -->
+        <section class="background-black">
+            <div class="section-wrapper">
+                <ul class="text-white">
+                    <li><a href="#activespeakers">Kompleti razglasa</a></li>
+                    <li><a href="#activespeakers">Aktivni zvučnici</a></li>
+                    <li><a href="#activespeakers">Pasivni zvučnici</a></li>
+                    <li><a href="#activespeakers">Monitorski zvučnici</a></li>
+                    <li><a href="#activespeakers">Pojačala</a></li>
+                    <li><a href="#activespeakers">Miksete</a></li>
+                    <li><a href="#activespeakers">Rasvjeta</a></li>
+                    <li><a href="#activespeakers">Kablovi</a></li>
+                    <li><a href="#activespeakers">Adapteri</a></li>
+                    <li><a href="#activespeakers">Dodatna oprema</a></li>
+                    <li><a href="#activespeakers">Torbe</a></li>
+                </ul>
             </div>
         </section>
     </div>
