590bbed348e798abdc6ad9a79912e91b4196474d
VavraLuka <luka123vavra@gmail.com>
2023-03-15 18:53:10 +0100

    Update

--- index.php
+++ index.php
@@ -2,10 +2,53 @@
 <html>
 <head>
     <title>Audio trgovina</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <?php
-    echo "Ovo je audio trgovina!";
-    ?>
+    <div class="page-wrapper">
+        <!--  -->
+        <section>
+            <div class="section-wrapper">
+                <div class="left">
+                    <ul>
+                        <li><a href="#contact">Kontaktiraj nas</a></li>
+                        <li><a href="#about">O nama</a></li>
+                    </ul>
+                </div>
+                <div class="center">
+                    <p><a href="#index">Logo</a></p>
+                </div>
+                <div class="right">
+                    <ul>
+                        <li><a href="#warranty">3 godine jamstva</a></li>
+                        <li><a href="#returns">Povrat robe</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="section-wrapper">
+                <div>
+                    <ul>
+                        <li><a>Top ponude</a></li>
+                        <li><a>Novo</a></li>
+                        <li><a>Najprodavanije</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <p>Search bar</p>
+                </div>
+                <div>
+                    <ul>
+                        <li><a>Odabir valute</a></li>
+                        <li><a>Profil</a></li>
+                        <li><a>Favoriti</a></li>
+                        <li><a>Košarica</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </div>
 </body>
 </html>
